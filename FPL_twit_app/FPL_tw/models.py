5f2e3ab3d32a79a79a8eb9b831b1fc41e6ad111d
Wiktor90 <w-witkowski90@o2.pl>
2020-10-29 16:12:48 +0100

    model update

--- FPL_twit_app/FPL_tw/models.py
+++ FPL_twit_app/FPL_tw/models.py
@@ -1,14 +1,9 @@
 from django.db import models
-from PIL import Image
 
 class Club(models.Model):
     name = models.TextField(max_length=100) # real name of club
     screen_name = models.CharField(max_length=100) # tweeter screen-name of club official profile
-    image = models.ImageField(default='img.png', upload_to='img')
+    kind = models.CharField(max_length=100, default="club")
 
     def __str__(self):
         return self.name
-
-#model Services:
-#SkySportsPL
-#premierleague
